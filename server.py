3ce7f6524e9d6cc24fbed667cd1c70749362ad4b
Unic0rn <ilyakotikov5@gmail.com>
2022-02-15 17:35:07 +0300

    Added /login, /success

--- server.py
+++ server.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин)', validators=[DataRequired()])
+    password = PasswordField('Пароль)', validators=[DataRequired()])
+    submit = SubmitField('Войти')
 
 
 app = Flask('Sus')
@@ -32,6 +41,19 @@ def answer():
              'profession': 'Импостер', 'sex': 'Подвижный грунтовый ракетный комплекс 15П645 (РСД-10) «Пионер»',
              'motivation': 'секретно', 'ready': True}
     return render_template('answer.html', **param)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Аварийный доступ', form=form)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 
 app.run(host='localhost', port=8080, debug=True)
